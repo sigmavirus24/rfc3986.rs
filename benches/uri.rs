c3188ff425ab79ac737b39dece502e0ce7e433d7
Ian Cordasco <graffatcolmingov@gmail.com>
2017-01-08 06:45:14 -0600

    Add benchmarks and run them on Travis CI

---
+++ benches/uri.rs
@@ -0,0 +1,14 @@
+#![feature(test)]
+extern crate test;
+
+use test::Bencher;
+use rfc3986::uri::Uri;
+
+#[bench]
+fn benchmark_url_parsing(b: &mut Bencher) {
+    b.iter(|| {
+        Uri::from_str(
+            "https://username:password@github.com/path/to?query=foo#fragment"
+        )
+    });
+}
